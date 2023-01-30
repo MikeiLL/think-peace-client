7789fe878d0238a533296b1d338ee8521279fce0
mikeill <mike@mzoo.org>
2023-01-30 09:48:57 -0600

    Remove Toastify.

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import { Welcome } from "./pages/welcome";
 import { Wish } from "pages/wish";
 import { Wishes } from "pages/wishes";
 import Screen from "pages/wishes/screen";
-import "react-toastify/dist/ReactToastify.css";
-import { ToastContainer } from "react-toastify";
 
 const router = createBrowserRouter([
   {
@@ -29,19 +27,6 @@ const router = createBrowserRouter([
 const App = () => {
   return (
     <>
-      <ToastContainer
-        position="top-center"
-        autoClose={false}
-        hideProgressBar={false}
-        newestOnTop={true}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="colored"
-        limit={6}
-      />
       <RouterProvider router={router} />
     </>
   );
