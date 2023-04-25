7f6967f3e3a6f017b93118d85b208548a20884bc
mikeill <mike@mzoo.org>
2023-04-25 07:23:23 -0500

    Remove Hold until wish is done.

--- src/pages/wish/index.tsx
+++ src/pages/wish/index.tsx
@@ -42,7 +42,7 @@ export const Wish = () => {
             className="h-full cursor-pointer py-4 px-6 flex flex-col items-center justify-center"
           >
             <h4 className="text-2xl text-white text-center">
-                Hi! Touch and hold to make a wish. Hold until wish is done.
+                Hi! Touch and hold to make a wish.
             </h4>
           </div>
         ) : (
