cb3a8c0082ea99d3e99c73c12717afa30681a51f
mikeill <mike@mzoo.org>
2023-06-02 13:20:21 -0500

    Remove all bullets and hypens from about page.

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -37,11 +37,11 @@ export const About = () => {
           <div className="ml-6">
             <p className="mb-2">Send a wish:</p>
             <ul className="mb-2 ml-4 list-none">
-              <li className="before:content-['–']"> &nbsp;In the menu at the top right, touch and hold Send a new wish</li>
-              <li className="before:content-['–']"> &nbsp;Continue to hold the screen while making the wish</li>
-              <li className="before:content-['–']"> &nbsp;Release the screen to send the wish</li>
-              <li className="before:content-['–']"> &nbsp;When you see the Thank You window, select
-                <ul className="mb-2 ml-8 list-disc">
+              <li> &nbsp;In the menu at the top right, touch and hold Send a new wish</li>
+              <li> &nbsp;Continue to hold the screen while making the wish</li>
+              <li> &nbsp;Release the screen to send the wish</li>
+              <li> &nbsp;When you see the Thank You window, select
+                <ul className="mb-2 ml-8 list-none">
                   <li>the location of the wish source</li>
                   <li>the destination of the wish</li>
                   <li>the kind of wish</li>
@@ -55,9 +55,9 @@ export const About = () => {
             </ul>
             <p className="mb-2">See today's wishes:</p>
             <ul className="mb-2 ml-4  list-none">
-              <li className="before:content-['–']"> &nbsp;In the menu at the top right, select Today's wishes</li>
-              <li className="before:content-['–']"> &nbsp;Use the top controls to
-                <ul className="mb-2 ml-8 list-disc">
+              <li> &nbsp;In the menu at the top right, select Today's wishes</li>
+              <li> &nbsp;Use the top controls to
+                <ul className="mb-2 ml-8 list-none">
                   <li>see the wishes and maps</li>
                   <li>enable visualizations and music that respond to wish activity</li>
                 </ul>
@@ -71,14 +71,14 @@ export const About = () => {
           <h2 className="mt-6 mb-2 text-2xl">Credits</h2>
 
           <ul className="mb-2 ml-4 list-none">
-            <li className="before:content-['–']"> &nbsp;Scott Anthony - Founder</li>
-            <li className="before:content-['–']"> &nbsp;Abby Strugger - Art director</li>
-            <li className="before:content-['–']"> &nbsp;Mike iLL - Technical design</li>
-            <li className="before:content-['–']"> &nbsp;Chris Angelico - Technical design</li>
-            <li className="before:content-['–']"> &nbsp;Faith Ekanem - Technical design</li>
-            <li className="before:content-['–']"> &nbsp;Oreoluwa Muzzamil - Technical design</li>
-            <li className="before:content-['–']"> &nbsp;Rebecca Turner - Copywriter</li>
-            <li className="before:content-['–']"> &nbsp;Catharine Alexander - Editor</li>
+            <li> &nbsp;Scott Anthony - Founder</li>
+            <li> &nbsp;Abby Strugger - Art director</li>
+            <li> &nbsp;Mike iLL - Technical design</li>
+            <li> &nbsp;Chris Angelico - Technical design</li>
+            <li> &nbsp;Faith Ekanem - Technical design</li>
+            <li> &nbsp;Oreoluwa Muzzamil - Technical design</li>
+            <li> &nbsp;Rebecca Turner - Copywriter</li>
+            <li> &nbsp;Catharine Alexander - Editor</li>
           </ul>
 
           <hr id="support" className="my-6"/>
