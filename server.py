a0f4244b6cc47c9548cb262447b041bdca738e13
mikeill <mike@mzoo.org>
2023-06-07 09:45:55 -0500

    Add REACT_APP_GOOGLE_API_KEY to wishes page.

--- server.py
+++ server.py
@@ -1,32 +1,12 @@
 from flask import Flask, request, jsonify, Response, render_template
 from dotenv import load_dotenv
 import os
+import requests
 
 load_dotenv()
 
 app = Flask(__name__, static_url_path='', static_folder='build', template_folder="build/templates")
 app.config['DEBUG'] = True
-
-@app.route("/sandbox")
-def sandbox():
-    og_description="Coming soon."
-    page_title="Wishes!!!!!!!"
-    meta_description="The Wishes page."
-    og_url="https://thinkpeace.app/wishes"
-    og_image="https://www.thinkpeace.app/comingsoon.gif"
-    return render_template("index.html",
-      og_description=og_description,
-      page_title=page_title,
-      og_image=og_image,
-      meta_description=meta_description,
-      og_url=og_url,
-      REACT_APP_GOOGLE_API_KEY=os.environ.get("REACT_APP_GOOGLE_API_KEY")
-    )
-
-
-@app.route("/")
-def index():
-    return app.send_static_file('index.html')
 
 @app.route("/wishes")
 def wishes():
@@ -35,8 +15,23 @@ def wishes():
     meta_description="The Wishes page."
     og_url="https://thinkpeace.app/wishes"
     og_image="https://www.thinkpeace.app/comingsoon.gif"
-    return render_template("wishes.html",
-      og_description=og_description, page_title=page_title, og_image=og_image, meta_description=meta_description, og_url=og_url)
+    pin = request.args.get('pin')
+    theme = request.args.get('theme')
+    r = requests.get('http://think-peace.herokuapp.com/wishes')
+    wishes = r.json()
+    wishes[0].get('_id')
 
+    return render_template("wishes.html",
+      REACT_APP_GOOGLE_API_KEY = os.getenv('REACT_APP_GOOGLE_API_KEY'),
+      pin=pin, theme=theme, og_description=og_description, page_title=page_title, og_image=og_image, meta_description=meta_description, og_url=og_url)
+
+@app.route("/")
+@app.route("/artists")
+@app.route("/contribute")
+@app.route("/about")
+@app.route("/wish")
+def index(path=None):
+    print("path", path)
+    return app.send_static_file('index.html')
 
 app.run(port=3000)
