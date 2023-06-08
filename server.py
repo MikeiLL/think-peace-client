5015ff4d1d601b8bfb4ec7a5a69e649e164ae748
mikeill <mike@mzoo.org>
2023-06-08 12:59:54 -0500

    Dynamic og tags based on shared wish.

--- server.py
+++ server.py
@@ -10,11 +10,11 @@ app.config['DEBUG'] = True
 
 @app.route("/wishes")
 def wishes():
-    og_description="Coming soon."
-    page_title="Wishes!!!!!!!"
-    meta_description="The Wishes page."
+    og_description="Activate your own peace today, then give it away. Do it again tomorrow. Peace is fragile; we need to find ways to activate it every day. "
+    page_title="Think Peace | Activate your own peace today. Then give it away."
     og_url="wishes"
     og_image="thinkpeacebanner1200x630.png"
+    og_title="Here is a wish for you!"
     pin = request.args.get('pin')
     theme = request.args.get('theme') or 'prototype'
     hashTag = None
@@ -23,6 +23,7 @@ def wishes():
       hashTag = wish.get('hashTag')[1:]
 
     if (hashTag is not None):
+      og_title="Here is a wish for #" + hashTag + ' on Think Peace.'
       if (os.path.exists('build/themes/' + theme + '/images/og/' + hashTag.lower() + '.png')):
         # maybe eventually we can dynamically build share files
         # for now, allow artist to upload their own share files
@@ -33,7 +34,7 @@ def wishes():
 
     return render_template("wishes.html",
       REACT_APP_GOOGLE_API_KEY = os.getenv('REACT_APP_GOOGLE_API_KEY'),
-      pin=pin, theme=theme, og_description=og_description, page_title=page_title, og_image=og_image, meta_description=meta_description, og_url=og_url)
+      pin=pin, og_title=og_title, theme=theme, og_description=og_description, page_title=page_title, og_image=og_image, meta_description=meta_description, og_url=og_url)
 
 @app.route("/")
 @app.route("/artists")
